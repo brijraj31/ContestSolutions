a94fcf6f3655f4d1471324141d7c3c1739bf069c
brijraj31 <22271048+brijraj31@users.noreply.github.com>
2020-03-05 03:22:13 +0530

    Update InputOutputTemplate.java

--- InputOutputTemplate.java
+++ InputOutputTemplate.java
@@ -229,7 +229,7 @@ public class ${NAME} {
 
         private static int[] readIntArray(InputReader in, int size) {
             int[] array = new int[size];
-            for (int i = 1; i < size; i++)
+            for (int i = 0; i < size; i++)
                 array[i] = in.readInt();
             return array;
         }
